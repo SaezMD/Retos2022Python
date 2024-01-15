22b3845468c43a3997920f2d13de2346ad986106
SaezMD <saulsaezrodriguez@gmail.com>
2024-01-15 12:22:46 +0100

    converted dates to numbers

--- 16python.py
+++ 16python.py
@@ -20,22 +20,58 @@ def leapYearsBetweenDates(date1: str, date2: str) -> int:
     print(leapYears)
     return leapYears
 
+def dayAsNumber(dateFormat: str) -> int:
+    """function to convert the date formated to int number as in Excel. Ref 0000 = 1600"""
+    dictMonthslengh= {
+        1 : 31,
+        2 : 28, 
+        3 : 31,
+        4 : 30,
+        5 : 31,
+        6 : 30,
+        7 : 31,
+        8 : 31,
+        9 : 30,
+        10 : 31,
+        11 : 30,
+        12 : 31
+    }
+
+    #Calculate years and leap years
+    year = int(dateFormat[-4:]) # Remove the actual year because is not completed
+    backYear = dateFormat[:-4] + str(year-1)
+    #print(backYear)
+    leapYears = leapYearsBetweenDates("01/01/1600",backYear)
+    yearDiff = int(backYear[-4:]) - 1600
+    years = (yearDiff - leapYears) * 365 + leapYears * 366
+
+    #Calculate months
+    month = int(dateFormat[3:5])
+    i = month - 1
+    daysInMonth = 0 
+    while i > 0:
+        #print(dictMonthslengh[i])
+        daysInMonth += dictMonthslengh[i]
+        i -= 1
+    #print(daysInMonth)
+    months = daysInMonth
+
+    #Calculate days
+    days =  int(dateFormat[:2]) 
+
+    #All sum:
+    allDays = days + months + years
+    print(allDays)
+    return allDays
+
+dayAsNumber("02/12/2020")
+
+
+
+
 def difDates(date1: str, date2: str) -> int:
     """function to get the difference between 2 dates"""
-    dictMonthslengh= {
-        "01" : 31,
-        "02" : 28, 
-        "03" : 31,
-        "04" : 30,
-        "05" : 31,
-        "06" : 30,
-        "07" : 31,
-        "08" : 31,
-        "09" : 30,
-        "10" : 31,
-        "11" : 30,
-        "12" : 31
-    }
+
 
     #Diff days
     days = int(date1[:2]) - int(date2[:2])
@@ -43,6 +79,7 @@ def difDates(date1: str, date2: str) -> int:
     #Diff months
     months = int(date1[3:5]) - int(date2[3:5])
     for i in range(abs(months)):
+        continue
         
 
     #Diff years
@@ -65,3 +102,4 @@ difDates("01/01/2020", "01/01/2021")
 
 leapYearsBetweenDates("01/01/2020", "01/01/2027")
 leapYearsBetweenDates("01/01/0499", "01/01/0501")
+leapYearsBetweenDates("01/01/1600", "01/01/2025")
