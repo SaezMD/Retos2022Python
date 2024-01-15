84a5965e6167b8a9d8456cc45679bfb038cd8aba
SaezMD <saulsaezrodriguez@gmail.com>
2024-01-15 19:43:02 +0100

    fixed for words with symbols

--- 17python.py
+++ 17python.py
@@ -4,13 +4,19 @@ Crea una función que reciba un String de cualquier tipo y se encargue de poner 
  * - No se pueden utilizar operaciones del lenguaje que lo resuelvan directamente.
 """
 
+
 def firstInUpper(string: str) -> str:
     """function to change first character of a word to uppercase"""
+    import re
     wordsList =  string.split()
     resultList = []
 
     for i in wordsList:
-        wordUp = i[:1].upper() + i[1:]
+        if re.match('^\W', i[:1]):
+            wordUp =  i[0:1] + i[1:2].upper() + i[2:]
+        else:
+            wordUp = i[:1].upper() + i[1:]
+        
         resultList.append(wordUp)
 
     result = " ".join(resultList)
@@ -19,3 +25,9 @@ def firstInUpper(string: str) -> str:
 
 
 firstInUpper("hola coca cola, por la carretera.")
+
+firstInUpper("hola coca cola, ¿cómo estás?")
+
+firstInUpper("el niño es un terrorista, ¿cómo estás?")
+
+firstInUpper("¡el ñu bebe carne!, ¿cómo estás?")
