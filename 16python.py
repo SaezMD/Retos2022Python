6be502d9b01331f1f5f587c43c72f4020742bb45
SaezMD <saulsaezrodriguez@gmail.com>
2024-01-15 13:38:51 +0100

    #16 - Python

--- 16python.py
+++ 16python.py
@@ -6,18 +6,61 @@
  * - La diferencia en días será absoluta (no importa el orden de las fechas).
  * - Si una de las dos cadenas de texto no representa una fecha correcta se lanzará una excepción.
 """
+dictMonthslengh= {
+        1 : 31,
+        2 : 28, 
+        3 : 31,
+        4 : 30,
+        5 : 31,
+        6 : 30,
+        7 : 31,
+        8 : 31,
+        9 : 30,
+        10 : 31,
+        11 : 30,
+        12 : 31
+    }
+
+def checkDatesFormats(date: str) -> bool:
+    import re
+    # All numbers must be possitives, only 2 "/" signs, 
+    if not re.match("^\d{2}\/\d{2}\/\d{4}$", date):
+        raise Exception("Sorry, date format is not correct. Format should be: 'dd/MM/yyyy'")
+    # months <12, days <31 
+    month = int(date[3:5])
+    if month > 12:
+        raise Exception("Sorry, wrong month number. Max: 12")
+    
+    #month limit by dict
+    monthLimit = dictMonthslengh[month]
+    day = int(date[:2])
+    if day > monthLimit:
+        raise Exception(f"Sorry, too many days in the month. Max for this month({month}): {monthLimit}")
+
+    # Years over 1600 DC
+    if int(date[-4:]) < 1600:
+        print(f"Some date is too old. Try with dates after 1600 DC")
+        return False
+
+    return True
+
+def leapYear(date: str) -> bool:
+    year = int(date[-4:])
+    if year % 4 == 0:
+        if year % 100 != 0 or year % 400 == 0:
+            return True
+    return False
 
 def leapYearsBetweenDates(date1: str, date2: str) -> int:
     """function to get number of leap years between 2 dates"""
     year01 = int(date1[-4:])
     year02 = int(date2[-4:])
     leapYears = 0 
-
-    for i in range(year01,year02):
+    for i in range(year01,year02+1):
         if i % 4 == 0:
             if i % 100 != 0 or i % 400 == 0:
                 leapYears += 1
-    print(leapYears)
+    #print(leapYears)
     return leapYears
 
 def dayAsNumber(dateFormat: str) -> int:
@@ -38,11 +81,11 @@ def dayAsNumber(dateFormat: str) -> int:
     }
 
     #Calculate years and leap years
-    year = int(dateFormat[-4:]) # Remove the actual year because is not completed
-    backYear = dateFormat[:-4] + str(year-1)
+    year = int(dateFormat[-4:]) 
+    backYear = dateFormat[:-4] + str(year-1) # Remove the actual year because is not completed
     #print(backYear)
-    leapYears = leapYearsBetweenDates("01/01/1600",backYear)
-    yearDiff = int(backYear[-4:]) - 1600
+    leapYears = leapYearsBetweenDates("01/01/2012",backYear) #Calculate number of leap days from last year until 1600
+    yearDiff = int(backYear[-4:]) - 1600 # Count from 1600
     years = (yearDiff - leapYears) * 365 + leapYears * 366
 
     #Calculate months
@@ -50,29 +93,45 @@ def dayAsNumber(dateFormat: str) -> int:
     i = month - 1
     daysInMonth = 0 
     while i > 0:
-        #print(dictMonthslengh[i])
         daysInMonth += dictMonthslengh[i]
         i -= 1
     #print(daysInMonth)
     months = daysInMonth
+    #Current leap year
+    currentLeap = 0 
+    if leapYear(dateFormat) and month > 2:
+        currentLeap = 1
 
     #Calculate days
     days =  int(dateFormat[:2]) 
 
     #All sum:
-    allDays = days + months + years
-    print(allDays)
+    allDays = days + months + years + currentLeap
+    #print(allDays)
     return allDays
 
-dayAsNumber("02/12/2020")
 
+def difDatesNumb(date1: str, date2: str) -> int:
+    """function to get the difference as number between 2 dates"""
+    #Check date format
+    if checkDatesFormats(date1) and checkDatesFormats(date2):
+        print("dates OK")
 
+    #Diff between dates
+    diffDates = abs(dayAsNumber(date1) - dayAsNumber(date2))
+    print(diffDates)
+    return diffDates
 
+difDatesNumb("01/01/2020", "33/24/2020")
+difDatesNumb("01/01/2020", "01/02/2020")
+difDatesNumb("01/01/2020", "02/02/2020")
+difDatesNumb("01/01/2020", "02/03/2020")
+difDatesNumb("01/01/2020", "01/01/2021")
+difDatesNumb("01/01/2000", "01/01/2026")
+difDatesNumb("01/01/1492", "01/01/2026")
 
 def difDates(date1: str, date2: str) -> int:
     """function to get the difference between 2 dates"""
-
-
     #Diff days
     days = int(date1[:2]) - int(date2[:2])
     
@@ -96,8 +155,6 @@ def difDates(date1: str, date2: str) -> int:
 difDates("01/01/2020", "02/02/2020")
 difDates("01/01/2020", "02/02/2020")
 difDates("01/01/2020", "01/01/2021")
-
-
 
 
 leapYearsBetweenDates("01/01/2020", "01/01/2027")
