8b4bc2e139e09a331bd52d0c3db8e667685cab7b
SaezMD <saulsaezrodriguez@gmail.com>
2024-01-22 14:11:20 +0100

    create file

---
+++ 33python.py
@@ -0,0 +1,10 @@
+#33 EL SEGUNDO
+"""
+Dado un listado de números, encuentra el SEGUNDO más grande
+"""
+
+def secondBiggest(listNumbs: list)-> int:
+    """function to return the second biggest number"""
+    
+
+    return print()
