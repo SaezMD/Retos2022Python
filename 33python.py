f9e0a30e3653a8c8efc0819d94ae46255d2c3609
SaezMD <saulsaezrodriguez@gmail.com>
2024-01-22 14:13:22 +0100

    #33 - Python

--- 33python.py
+++ 33python.py
@@ -3,8 +3,14 @@
 Dado un listado de números, encuentra el SEGUNDO más grande
 """
 
-def secondBiggest(listNumbs: list)-> int:
+def secondBiggest(listNumb: list)-> int:
     """function to return the second biggest number"""
-    
+    for i in range(0,len(listNumb)):
+        for j in range(i+1, len(listNumb)):
+            if listNumb[i] <= listNumb[j]:
+                listNumb[i],listNumb[j] = listNumb[j], listNumb[i]
 
-    return print()
+    return print(listNumb[1])
+
+secondBiggest([2, 20, 1, 10, 8, 9, 100, 50, 5])
+secondBiggest([2, 20, 1, 10, 8, 9, 50, 5])
