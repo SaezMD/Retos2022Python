3f2c53487e321bed8e859587e54f49f2149895d9
SaezMD <saulsaezrodriguez@gmail.com>
2024-01-23 16:34:50 +0100

    #43 - Python

--- 43python.py
+++ 43python.py
@@ -5,4 +5,18 @@
  * - En caso contrario retornará un error.
 """
 
+def changeBetweenCelsiusFarenheit(temperature: str)-> str:
+    """"function to convert from Fahrenheit to Celsius and viceversa"""
+    #check if entry is OK
+    if temperature[-2:] == "ºC":
+        fahren = int(temperature[0:-2]) * 1.8 + 32
+        return print(f"{temperature} are: {'{:0.2f}'.format(fahren)}ºF")
+    elif temperature[-2:] == "ºF":        
+        celsius = (int(temperature[0:-2]) - 32) / 1.8
+        return print(f"{temperature} are: {'{:0.2f}'.format(celsius)}ºC")
+    else:
+       raise Exception("Error in the units! Please check it.")
+
+changeBetweenCelsiusFarenheit("20ºC")
+changeBetweenCelsiusFarenheit("200ºF")
 
