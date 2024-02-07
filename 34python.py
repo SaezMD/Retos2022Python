b8b60c4ed3b0f19457a45bdadd2beb8fa9764753
SaezMD <saulsaezrodriguez@gmail.com>
2024-02-07 12:47:02 +0100

    #34 - Python

--- 34python.py
+++ 34python.py
@@ -21,6 +21,19 @@ listElements = [
     "agua"
 ]
 
+listElementsENG = [
+    "wood",
+    "wood",
+    "fire",
+    "fire",
+    "earth",
+    "earth",
+    "metal",
+    "metal",
+    "water",
+    "water"
+]
+
 listAnimals = [
     "rata", 
     "buey", 
@@ -36,11 +49,28 @@ listAnimals = [
     "cerdo"
 ]
 
+listAnimalsENG = [
+    "rat", 
+    "ox", 
+    "tiger", 
+    "rabbit", 
+    "dragon", 
+    "snake", 
+    "horse", 
+    "sheep", 
+    "monkey", 
+    "rooster", 
+    "dog", 
+    "pig"
+]
+
 def chineseCycle(year: int)-> str:
+    """function to get the sexagenary cycle of thhe Chinese 60-year calendar cycle is based on the combinations of a 
+    cycle of ten heavenly stems and twelve earthly branches. Each year is named by a pair of one stem and one branch. """
     refYear = year-1924 
     elementYear = refYear % 10
     animalYear = refYear % 12
-    print(year,listAnimals[animalYear],listElements[elementYear])
+    print(year,listAnimals[animalYear],listElements[elementYear],listAnimalsENG[animalYear],listElementsENG[elementYear])
 
 chineseCycle(1924)
 chineseCycle(1950)
